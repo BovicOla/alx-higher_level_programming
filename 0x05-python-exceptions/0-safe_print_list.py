296c1d45affe8a59678da617a242146de1c52ab1
BovicOla <olaleyebode001@gmail.com>
2022-10-10 18:58:42 -0700

    Safe list Printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,7 @@ def safe_print_list(my_list=[], x=0):
             printed += 1
         except TypeError:
             continue
+        except ValueError:
+            continue
     print()
     return printed
