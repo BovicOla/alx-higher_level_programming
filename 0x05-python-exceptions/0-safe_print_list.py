37bd9d904d0e5376181a7025d5f9180723447919
BovicOla <olaleyebode001@gmail.com>
2022-10-10 19:10:43 -0700

    Safe list Printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,8 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             printed += 1
         except ValueError:
+            return False
+        except TypeError:
             continue
     print()
     return printed
