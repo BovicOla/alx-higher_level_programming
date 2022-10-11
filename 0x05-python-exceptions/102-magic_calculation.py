ce5226d38da99ab8daf61201546ba408f3ccfb8a
BovicOla <olaleyebode001@gmail.com>
2022-10-10 19:58:46 -0700

    Magical calc

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 var += (a**b)/i
-        except ZeroDivisionError:
+        except:
             var = (b + a)
             break
     return var
